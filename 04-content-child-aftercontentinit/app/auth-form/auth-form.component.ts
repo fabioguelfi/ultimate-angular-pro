b2b1b4e7489440643ed2892b3bd903e92812a228
fabio guelfi <fabioguelfiunix@gmail.com>
2018-07-09 20:11:16 -0300

    @ContentChild and ngAfterContentInit

--- 04-content-child-aftercontentinit/app/auth-form/auth-form.component.ts
+++ 04-content-child-aftercontentinit/app/auth-form/auth-form.component.ts
@@ -1,11 +1,17 @@
-import { Component, Output, EventEmitter, ContentChild, AfterContentInit } from '@angular/core';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ContentChild,
+  AfterContentInit
+} from "@angular/core";
 
-import { AuthRememberComponent } from './auth-remember.component';
+import { AuthRememberComponent } from "./auth-remember.component";
 
-import { User } from './auth-form.interface';
+import { User } from "./auth-form.interface";
 
 @Component({
-  selector: 'auth-form',
+  selector: "auth-form",
   template: `
     <div>
       <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
@@ -28,7 +34,6 @@ import { User } from './auth-form.interface';
   `
 })
 export class AuthFormComponent implements AfterContentInit {
-
   showMessage: boolean;
 
   @ContentChild(AuthRememberComponent) remember: AuthRememberComponent;
@@ -37,12 +42,13 @@ export class AuthFormComponent implements AfterContentInit {
 
   ngAfterContentInit() {
     if (this.remember) {
-      this.remember.checked.subscribe((checked: boolean) => this.showMessage = checked);
+      this.remember.checked.subscribe(
+        (checked: boolean) => (this.showMessage = checked)
+      );
     }
   }
 
   onSubmit(value: User) {
     this.submitted.emit(value);
   }
-
 }
